a5646a04bd9d3f7eb9138cca41f25b28fd288493
Sophia <schen9981@gmail.com>
2020-04-21 21:55:16 -0600

    add cross validation svm

--- code/model.py
+++ code/model.py
@@ -6,13 +6,57 @@ from scipy import ndimage
 from sklearn.svm import SVC
 import math
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from scipy.stats import sem
 
 
-# def train_svm():
-#     # support vector classifier with radial basis kernel
-#     svc = SVC(kernel='rbf')
+def scores_cross_validation_svm(svc, data, labels, k):
+    '''
+        evaluates the k-fold cross validation scores: trains the model using k-1 of the folds (is subsets of our data), 
+        then is validated using the remain part of the data
 
+        svc: our support vector classifier
+        k: number of splits
+        data: our images to train on
+        labels: labels for each image
+    '''
+    # initialize KFold, which divides all the samples into k groups of samples (folds) of equal sizes
+    kf = KFold(n_splits=k, random_state=0, shuffle=True)
 
+     # iterate through each of the possible splits and train
+    scores = []
+    for i in range(k):
+        # get next split indices
+        split_train_ind, split_test_ind = next(kf.split(data))
+
+        # split data
+        train_data_split = data[split_train_ind]
+        train_label_split = labels[split_train_ind]
+
+        test_data_split = data[split_test_ind]
+        test_label_split = labels[split_test_ind]
+
+        # fit data on training splits
+        model = svc.fit(train_data_split, train_label_split)
+
+        # predict on test splits and evaluate score (ie accuracy)
+        predict = svc.predict(test_data_split)
+        scores.append(model.score(test_data_split, test_label_split))
+    
+    print(scores)
+
+def evaluate_performance(svc, train_data, train_labels, test_data, test_labels):
+    '''
+        evaluates performance of our svc
+    '''
+    # fit our svc (which was trained using kfold cross validation) to training data
+    svc.fit(train_data, train_labels)
+
+    # determine train accuracy
+    print("Training Accuracy: ", svc.score(train_data, train_labels))
+
+    # determine test accuracy
+    print("Testing Accuracy", svc.score(test_data, test_labels))
 
 
 def visualize_imgs(data_sample):
@@ -29,6 +73,7 @@ def visualize_imgs(data_sample):
         # plot the images
         p = fig.add_subplot(fig_size, fig_size, i + 1)
         p.imshow(data, cmap=plt.cm.gray)
+        p.axis('off')
     
     plt.show()
 
@@ -42,10 +87,23 @@ def main():
     image_dir = '../data/genki4k/files'
     labels_dir = '../data/genki4k/labels.txt'
 
+    
+
+    # read in data
     data_sample, labels, headpose = read_raw_data(image_dir, labels_dir)
-    visualize_imgs(data_sample)
+    # visualize_imgs(data_sample)
+
+    # train-test split
+    train_data, train_labels, train_headpose, test_data, test_labels, test_headpose = train_test_split(data_sample, labels, headpose)
+
+    # initialize support vector classifer with linear kernel 
+    svc = SVC(kernel='linear')
+
+    # k-fold cross-validation
+    k = 5
+    scores_cross_validation_svm(svc, train_data, train_labels, k)
+    evaluate_performance(svc, train_data, train_labels, test_data, test_labels)
         
-    # train_data, train_labels, train_headpose, test_data, test_labels, test_headpose = train_test_split(data_sample, labels, headpose)
 
 if __name__ == '__main__':
     main()
